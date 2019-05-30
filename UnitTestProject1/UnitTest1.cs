2badc06246ecb4d4462e14f0aff1845dc319f79d
Pritika <t-prpriy@microsoft.com>
2019-05-30 18:41:39 +0530

    Add project files.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,49 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using simplecalc;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Program p = new Program();
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int a = 6;
+            int b = 3;
+            int ex = 9;
+            int r = p.Add(a, b);
+            Assert.AreEqual(r, ex);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int a = 6;
+            int b = 3;
+            int ex = 3;
+            int r = p.Sub(a, b);
+            Assert.AreEqual(r, ex);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int a = 6;
+            int b = 3;
+            int ex = 18;
+            int r = p.Mul(a, b);
+            Assert.AreEqual(r, ex);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            int a = 6;
+            int b = 3;
+            int ex = 2;
+            int r = p.Div(a, b);
+            Assert.AreEqual(r, ex);
+        }
+    }
+}
---
+++ simplecalc/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace simplecalc
+{
+   public class Program
+    {
+        public int Add(int x,int y)
+        {
+            return (x + y);
+        }
+        public int Sub(int x,int y)
+        {
+            return (x - y);
+        }
+        public int Mul(int x,int y)
+        {
+            return (x * y);
+        }
+        public int Div(int x,int y)
+        {
+            return (x / y);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a,b,c=0;
+            Console.WriteLine("Enter two numbers : ");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the operation: ");
+            Console.WriteLine("1.Add");
+            Console.WriteLine("2.Subtract");
+            Console.WriteLine("3.Multiply");
+            Console.WriteLine("4.Divide");
+            int d = Convert.ToInt32(Console.ReadLine());
+            switch (d)
+            {
+                case 1:
+                    c = a + b;
+                    Console.WriteLine("hi");
+                    break;
+                case 2:
+                    c = a - b;
+                    break;
+                case 3:
+                    c = a * b;
+                    break;
+                case 4:
+                    c = a / b;
+                    break;
+                default:
+                    Console.WriteLine("Invalid option!");
+                    break;
+
+            }
+            Console.WriteLine("The result is " +c);
+        }
+    }
+}
